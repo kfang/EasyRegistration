4a99a586f26845b7dc8b8526d8dc094549b7ab6c
hkrfang <hkr.fang@gmail.com>
2017-03-28 15:07:24 -0700

    add Registrant model

---
+++ src/main/scala/com/github/kfang/easyregistration/models/Registrant.scala
@@ -0,0 +1,28 @@
+package com.github.kfang.easyregistration.models
+
+import java.util.UUID
+
+import reactivemongo.bson.{BSONDateTime, BSONDocument, BSONDocumentReader, BSONDocumentWriter, BSONHandler, Macros}
+
+case class Registrant (
+  firstName: String,
+  lastName: String,
+  birthday: BSONDateTime,
+  gender: String,
+
+  allergies: Option[String],
+  comments: Option[String],
+
+  flags: Seq[String],     //TODO: enumerate different flags
+  parent: Option[String], //TODO: implement Parent model
+  extraInformation: Option[BSONDocument] = None,
+  _id: String = UUID.randomUUID().toString
+)
+
+object Registrant {
+
+  implicit val __bsf: BSONDocumentReader[Registrant]
+    with BSONDocumentWriter[Registrant]
+    with BSONHandler[BSONDocument, Registrant] = Macros.handler[Registrant]
+
+}
